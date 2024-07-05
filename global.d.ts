a2e7e42a244ad787bb319b901c96350c676c2c52
Claudio Lau <claudio.lau12@gmail.com>
2024-07-04 21:33:33 -0400

    remove package lock

--- global.d.ts
+++ global.d.ts
@@ -8,3 +8,27 @@ declare module "*.svg" {
 	const src: string;
 	export default src;
 }
+declare module "*.module.css" {
+	const classes: { readonly [key: string]: string };
+	export default classes;
+}
+declare module "*.module.scss" {
+	const classes: { readonly [key: string]: string };
+	export default classes;
+}
+declare module "*.module.sass" {
+	const classes: { readonly [key: string]: string };
+	export default classes;
+}
+declare module "*.module.less" {
+	const classes: { readonly [key: string]: string };
+	export default classes;
+}
+declare module "*.module.styl" {
+	const classes: { readonly [key: string]: string };
+	export default classes;
+}
+declare module "*.module.stylus" {
+	const classes: { readonly [key: string]: string };
+	export default classes;
+}
--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { pluginSass } from "@rsbuild/plugin-sass";
+import { pluginTypedCSSModules } from "@rsbuild/plugin-typed-css-modules";
 
 export default defineConfig({
-	plugins: [pluginReact(), pluginSass()],
+	plugins: [pluginReact(), pluginSass(), pluginTypedCSSModules()],
+	output: {
+		cssModules: {
+			auto: (resource) => {
+				return resource.includes(".module.") || resource.includes("shared/");
+			},
+		},
+	},
 });
